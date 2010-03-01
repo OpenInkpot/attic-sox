845a674ebf505dd6bc302a2a30bb719a05868be6
Alexander V. Nikolaev <avn@daemon.hole.ru>
2010-03-01 20:51:37 +0000

    Final switch to own autocrap

---
+++ autogen.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+rm -rf autom4te.cache
+rm -f aclocal.m4 ltmain.sh
+
+echo "Running aclocal..." ; aclocal -I m4 $ACLOCAL_FLAGS  || exit 1
+echo "Running autoheader..." ; autoheader || exit 1
+echo "Running autoconf..." ; autoconf || exit 1
+echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
+echo "Running automake..." ; automake --add-missing --copy --foreign || exit 1
+
+chmod +x ./configure
+exec ./configure $@
